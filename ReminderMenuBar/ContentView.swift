37eb852ccaef166b75cd9d87f8d6c6f735a7f5c9
João Mateus <joaomateusabm@yahoo.com.br>
2024-05-24 10:53:10 -0300

    Initial Commit

---
+++ ReminderMenuBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReminderMenuBar
+//
+//  Created by João Melo on 24/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReminderMenuBar/ReminderMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderMenuBarApp.swift
+//  ReminderMenuBar
+//
+//  Created by João Melo on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ReminderMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
